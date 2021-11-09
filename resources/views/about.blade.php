f83c8f03be978fb231d30a24d49c75b8c1320e2c
Jake <jakewarrenblack01@gmail.com>
2021-11-09 15:23:56 +0000

    Add routes/views/controllers for starter code

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,36 +4,25 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Register</title>
+        <title>About</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-        <!-- Styles -->
 
 
         <style>
             body {
                 font-family: 'Nunito', sans-serif;
-            };
+            },
+            h1{
+                color:white;
+            }
         </style>
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            <ul>
-                <li>
-                    <a href="/login">Login</a>
-                </li>
-                <li>
-                    <a href="/register">Register</a>
-                </li>
-                <li>
-                    <a href="/about">About</a>
-                </li>
-                <li>
-                    <a href="/profile">Profile</a>
-                </li>
-            </ul>
+            <h1>About</h1>
         </div>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,34 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/login', [LoginController::class, 'login']);
+
+// this is an alternative way of doing the above ^^
+// in this case we don't have to import anything, it's done inline
+// --------------------------------------------------------------- //
+// Route::get('about', 'App\Http\Controllers\AboutController@about');
+
+Route::get('/about', [LoginController::class, 'about']);
+Route::get('/register', [LoginController::class, 'register']);
+Route::get('/profile', [LoginController::class, 'profile']);
+
+
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/profile', function () {
+    return view('profile');
 });
